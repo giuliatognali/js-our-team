81e49564e2d121541de1255fde6712377a1b8a7f
giuliatognali <giuliatognali@gmail.com>
2023-03-07 14:55:12 +0100

    create obj

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 'use strict';
 
+const member1 = {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    img: 'wayne-barnett-founder-ceo.jpg'
+}
+
+const member2 = {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    img: 'angela-caroll-chief-editor.jpg'
+}
+const member3 = {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    img: 'walter-gordon-office-manager.jpg'
+}
+const member4 = {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    img: 'angela-lopez-social-media-manager.jpg'
+}
+const member5 = {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    img: 'scott-estrada-developer.jpg'
+}
+const member6 = {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    img: 'barbara-ramos-graphic-designer.jpg'
+}
